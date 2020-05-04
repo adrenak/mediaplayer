2b9a7e59dc637c6f0dd371357bf3db0b5fbd3bf6
Vatsal Ambastha <adrenak_@outlook.com>
2020-05-04 16:23:53 +0530

    Add IMediaPlayer

---
+++ Assets/Adrenak.Playback/IMediaPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Adrenak.MediaPlayer {
+    public interface IMediaPlayer {
+        void Open(string path, bool autoPlay);
+        void Play();
+        void Pause();
+
+        void Jump(int frameDelta);
+        void Jump(TimeSpan timeSpanDelta);
+        void Jump(float positionDetla);
+
+        void Seek(int frame);
+        void Seek(TimeSpan timeSpace);
+        void Seek(float position);
+
+        int CurrentFrame { get; }
+        float CurrentPosition { get; }
+        TimeSpan CurrentTimeSpan { get; }
+    }
+}
---
+++ Assets/Adrenak.Playback/UnityMediaPlayer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace Adrenak.MediaPlayer{
+    public class UnityMediaPlayer : MonoBehaviour, IMediaPlayer {
+        [SerializeField] VideoPlayer player;
+
+        public int CurrentFrame => throw new NotImplementedException();
+
+        public float CurrentPosition => throw new NotImplementedException();
+
+        public TimeSpan CurrentTimeSpan => throw new NotImplementedException();
+
+        public void Jump(int frameDelta) {
+            throw new NotImplementedException();
+        }
+
+        public void Jump(TimeSpan timeSpanDelta) {
+            throw new NotImplementedException();
+        }
+
+        public void Jump(float positionDetla) {
+            throw new NotImplementedException();
+        }
+
+        public void Open(string path, bool autoPlay) {
+            throw new NotImplementedException();
+        }
+
+        public void Pause() {
+            throw new NotImplementedException();
+        }
+
+        public void Play() {
+            throw new NotImplementedException();
+        }
+
+        public void Seek(int frame) {
+            throw new NotImplementedException();
+        }
+
+        public void Seek(TimeSpan timeSpace) {
+            throw new NotImplementedException();
+        }
+
+        public void Seek(float position) {
+            throw new NotImplementedException();
+        }
+    }
+}
