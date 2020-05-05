b09dac81bfdca0dc907b3c0f6072298b895cc33c
Vatsal Ambastha <adrenak_@outlook.com>
2020-05-05 17:50:51 +0530

    feat: Initial feature set for IMediaPlayer
    Support UPM
    Additions to IMediaPlayer:
    - Stop feature
    - OnError event
    Improve SampleMediaPlayer and scene
    Remove unused project dependencies

--- Assets/MediaPlayer/Samples/SampleMediaPlayerUI.cs
+++ Assets/MediaPlayer/Samples/SampleMediaPlayerUI.cs
@@ -5,30 +5,33 @@ using UnityEngine.UI;
 
 namespace Adrenak.MediaPlayer {
     public class SampleMediaPlayerUI : MonoBehaviour {
+        [SerializeField] InputField urlInput;
+        [SerializeField] Toggle autoPlay;
         [SerializeField] RawImage videoSurface;
         [SerializeField] Text status;
         [SerializeField] Image seekBar;
 
         IMediaPlayer player;
 
-        void Start() {
+        public void Load() {
             player = FindObjectOfType<UnityMediaPlayer>();
-            player.Open("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4", false);
+            player.Open(urlInput.text, autoPlay.isOn);
 
             player.OnReady += () => {
-                var texture = player.Texture;
+                var texture = player.MediaTexture;
                 videoSurface.texture = texture;
-                
+
                 var fitter = videoSurface.GetComponent<AspectRatioFitter>();
                 fitter.aspectRatio = (float)texture.width / texture.height;
 
                 StartCoroutine(ShowStatus("Ready", 1));
             };
+            player.OnError += error => StartCoroutine(ShowStatus(error.Message, 1));
             player.OnPlay += () => StartCoroutine(ShowStatus("Playing", 1));
             player.OnPause += () => StartCoroutine(ShowStatus("Paused", 1));
         }
 
-        IEnumerator ShowStatus(object msg, float delay = 0){
+        IEnumerator ShowStatus(object msg, float delay = 0) {
             status.text = msg.ToString();
             yield return new WaitForSeconds(delay);
             status.text = string.Empty;
@@ -48,14 +51,18 @@ namespace Adrenak.MediaPlayer {
                 player.Play();
         }
 
-        public void Forward(){
+        public void Stop() {
+            player.Stop();
+        }
+
+        public void Forward() {
             player.JumpTimeSpan(TimeSpan.FromSeconds(5));
-            StartCoroutine(ShowStatus("Jumping forward"));
+            StartCoroutine(ShowStatus("Jumping forward", 1));
         }
 
         public void Backward() {
             player.JumpTimeSpan(TimeSpan.FromSeconds(-5));
-            StartCoroutine(ShowStatus("Jumping backward"));
+            StartCoroutine(ShowStatus("Jumping backward", 1));
         }
     }
 }
